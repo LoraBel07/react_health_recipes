7cce9ea4e6d20ef911067e4f127daeb1a7910eda
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2022-08-17 11:26:25 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect } from "react";
 
 function App() {
+  const MY_ID = "c7adf8c0";
+  const MY_KEY = "b1d50e832a66cd1a64694436a988e652";
+
+  useEffect(async () => {
+    // const response = await fetch(`https://api.edamam.com/api/food-database/v2/parser?app_id=${MY_ID}&app_key=${MY_KEY}&nutrition-type=cooking`);
+    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=avocado&app_id=${MY_ID}&app_key=${MY_KEY}`);
+    const data = await response.json();
+    console.log(data);
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Diet</h1>
+      
     </div>
   );
 }
